7690394428382f428cd0113816fb5f6521a7b787
Oki-LM7 <ok13247@gmail.com>
2022-04-28 22:24:52 -0500

    "refactored  sql database ."

--- BankApp/banking_app/src/main/java/revature/banking_app/Data/SQL_Database.java
+++ BankApp/banking_app/src/main/java/revature/banking_app/Data/SQL_Database.java
@@ -1,5 +1,6 @@
 package revature.banking_app.Data;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,10 +11,10 @@ public class SQL_Database implements  iDatabase {
 
 
     @Override
-    public HashMap getUser(String username,String accountType) {
+    public HashMap getUser(String username, String accountType) {
 
 
-       return getAllUsers().get(username+accountType);
+        return getAllUsers().get(username + accountType);
 
 
     }
@@ -30,24 +31,27 @@ public class SQL_Database implements  iDatabase {
 
 
             ResultSet rs = statement.executeQuery(query);
-            HashMap users  = new HashMap<>();
+            HashMap users = new HashMap<>();
 
             // Step 2
             while (rs.next()) {
                 // user exists
                 HashMap<String, Object> user = new HashMap<>();
                 user.put("username", rs.getString("username")); // also just the username variable
-                user.put("password",  rs.getInt("password"));
+                user.put("password", rs.getInt("password"));
                 user.put("firstName", rs.getString("first_name"));
                 user.put("lastname", rs.getString("last_name"));
                 user.put("rank", rs.getString("rank"));
-                user.put("accountType", rs.getString("account_Type"));
+                user.put("accountType", rs.getString("account_type"));
+                user.put("accountStatus", rs.getString("account_status"));
+                user.put("activeStatus", rs.getString("active_status"));
                 user.put("owners", rs.getString("owners"));
                 user.put("balance", rs.getInt("balance"));
 
-                users.put(rs.getString("username")+rs.getString("account_Type"),user);
+                users.put(rs.getString("username") + rs.getString("account_Type"), user);
 
-            }  return users; // user does not exist
+            }
+            return users; // user does not exist
 
         } catch (SQLException e) {
             // TODO Auto-generated catch block
@@ -59,6 +63,31 @@ public class SQL_Database implements  iDatabase {
 
     @Override
     public void saveUserInfo(HashMap user, String username, String infoType) {
+        try {
+            // Step 1
+            String query = "Insert into " + infoType + " Values (?,?,?,?)";
+            PreparedStatement statement = ConnectionManager.getConnection().prepareStatement(query);
 
+            // Set the username filter value (ie the ?)
+            statement.setString(1, user.get("username").toString());
+            statement.setString(2, user.get("username").toString());
+            statement.setString(3, user.get("username").toString());
+            statement.setString(4, user.get("username").toString());
+            statement.setString(5, user.get("username").toString());
+            statement.setString(6, user.get("username").toString());
+            statement.setString(7, user.get("username").toString());
+            statement.setString(8, user.get("username").toString());
+            statement.setString(9, user.get("username").toString());
+            statement.setString(10, user.get("username").toString());
+
+
+
+
+        }  catch(SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
     }
+
+       }
+
 }
